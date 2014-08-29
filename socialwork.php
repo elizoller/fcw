cbf0dda93d231aaacd3b2ded56e65ebe20e80f71
elizoller <zoller.eli@gmail.com>
2014-08-28 20:21:44 -0400

    adding google analytics

--- socialwork.php
+++ socialwork.php
@@ -30,8 +30,20 @@ while ($row = $stmt1->fetch(PDO::FETCH_ASSOC)) {
       <script src="bootstrap/assets/js/html5shiv.js"></script>
       <script src="bootstrap/assets/js/respond.min.js"></script>
     <![endif]-->
-   <!--insert google analytics script-->
+   <!-- google analytics script-->
+   <script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-16674574-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
   </head>
 <body> 
 
